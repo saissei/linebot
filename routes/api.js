ef56f59972624eddefe1c27038664a1ccb894c1d
saissei <tariraritarara.214@gmail.com>
2019-06-12 18:46:21 +0900

    input:都道府県 output: 5日分の天気情報 bug: 日付が順不同, 同時に送信してしまうためLine側からエラーレスポンス発生

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const router = express.Router()
-const axios = require('axios')
-const crypto = require('crypto')
-const uri = 'https://api.line.me/v2/bot/message/reply'
-const signature = crypto.createHmac('sha256', process.env.ch_secret)
-const bearerToken = 'Bearer ' + process.env.access_token
-const header = {
-  'Content-Type': 'application/json; charset=UTF-8',
-  'X-Line-Signature': signature,
-  Authorization: bearerToken
-}
+const moment = require('moment')
+const line = require('../lib/line')
+const geo = require('../lib/geocoding')
+const wth = require('../lib/wether')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -22,26 +16,74 @@ router.get('/', (req, res, next) => {
 
 router.post('/webhook', (req, res, next) => {
   res.status(200).end()
+
   const receiveData = req.body.events
   let message = receiveData.filter(item => {
-    if (item.type === 'message') return item
+    return item.type === 'message'
   })
   message.map(async item => {
-    const data = {
-      replyToken: item.replyToken,
-      messages: [
-        {
-          type: 'text',
-          text: item.message.text
+    const searchAddress = item.message.text
+    const replyToken = item.replyToken
+    try {
+      const geoIp = await geo.getGeo(searchAddress)
+      const weather = await wth.describe(
+        geoIp.coordinate.lat,
+        geoIp.coordinate.lng
+      )
+      let wetherProperty = weather.data.list.filter(list => {
+        console.log(moment.unix(list.dt).format('YYYY-MM-DD HH:mm:ss'))
+        return moment.unix(list.dt).format('HH') === '09'
+      })
+      wetherProperty.map(async list => {
+        const date = moment.unix(list.dt).format('MM月DD日の天気')
+        const temp_min = list.main.temp_min - 273.15
+        const temp_max = list.main.temp_max - 273.15
+        const humidity = list.main.humidity
+        const pressure = list.main.pressure
+        let weatherDesc
+        switch (list.weather[0].icon) {
+          case '01d':
+            weatherDesc = '快晴'
+            break
+          case '02d':
+            weatherDesc = '晴れ'
+            break
+          case '03d':
+          case '04d':
+            weatherDesc = 'くもり'
+            break
+          case '09d':
+            weatherDesc = '小雨'
+            break
+          case '10d':
+            weatherDesc = '雨'
+            break
+          case '11d':
+            weatherDesc = '雷雨'
+            break
+          case '13d':
+            weatherDesc = '雪'
+            break
+          case '50d':
+            weatherDesc = '霧'
+            break
         }
-      ]
+        const message = `${date}\nてんき: ${weatherDesc}\nさいていきおん: ${temp_min}℃\nさいこうきおん: ${temp_max}℃\nしつど: ${humidity}%\n気圧: ${pressure}hPa`
+
+        await line
+          .sendMessage(replyToken, message)
+          .then(result => console.log('success'))
+          .catch(err => console.error(err))
+      })
+
+      /*await line
+        .sendMessage(replyToken, wetherProperty)
+        .then(result => console.log(result.data))
+        .catch(err => console.error(err))
+      */
+    } catch (err) {
+      console.error(err)
     }
-    console.log(header)
-    console.log(data)
-    await axios
-      .post(uri, data, { headers: header })
-      .then(result => console.log(result))
-      .catch(err => console.error(err))
   })
 })
 
