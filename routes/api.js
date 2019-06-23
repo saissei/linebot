bc5c4be2532858bc2383570cbbe2e806676c74f0
saissei <tariraritarara.214@gmil.com>
2019-06-24 03:08:35 +0900

    menuから起動、クイックメニューで位置の共有、共有から天気の取得まで。 BUG: ngrokで起動しているとポートが違うため内部APIが呼び出せず、ポート指定している..

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+const axios = require('axios')
 const router = express.Router()
 const moment = require('moment')
+const qs = require('qs')
 const line = require('../lib/line')
 const geo = require('../lib/geocoding')
 const wth = require('../lib/wether')
@@ -21,49 +23,94 @@ router.get('/', (req, res, next) => {
     .json({ status: 'success' })
 })
 
-router.post('/webhook', (req, res, next) => {
+router.post('/webhook', async (req, res, next) => {
   res.status(200).end()
 
-  const receiveData = req.body.events
-  let message = receiveData.filter(item => {
-    return item.type === 'message'
-  })
-  message.map(async item => {
-    const searchAddress = item.message.text
-    const replyToken = item.replyToken
-    try {
-      const geoIp = await geo.getGeo(searchAddress)
-      const weather = await wth.describe(
-        geoIp.coordinate.lat,
-        geoIp.coordinate.lng
-      )
-      let wetherProperty = weather.data.list.filter(list => {
-        console.log(moment.unix(list.dt).format('YYYY-MM-DD HH:mm:ss'))
-        return moment.unix(list.dt).format('HH') === '09'
+  try {
+    const receiveData = req.body.events
+    let message = receiveData.filter(item => {
+      return item.type === 'message'
+    })
+    if (message[0].message.type === 'text') {
+      axios
+        .post('http://localhost:3000/textMessage', message)
+        .then(result => console.log(result.data))
+        .catch(err => console.error(err.response.data))
+    } else if (message[0].message.type === 'location') {
+      console.log({
+        latitude: message[0].message.latitude,
+        longitude: message[0].message.longitude
       })
-      wetherProperty = wetherProperty.map(list => {
-        const date = moment.unix(list.dt).format('MM月DD日の天気')
-        const temp_min = Math.floor(list.main.temp_min - 273.15)
-        const temp_max = Math.floor(list.main.temp_max - 273.15)
-        const humidity = list.main.humidity
-        const pressure = list.main.pressure
-        const weatherDesc = wth.wetherDescription(list.weather[0].icon)
+      const weather = await axios.get('http://localhost:3000/weather', {
+        params: {
+          latitude: message[0].message.latitude,
+          longitude: message[0].message.longitude
+        },
+        paramsSerializer: params => {
+          return qs.stringify(params)
+        }
+      })
+    }
+  } catch (err) {
+    console.log(err)
+  }
+})
 
-        return (message = `${date}\nてんき: ${weatherDesc}\nさいていきおん: ${temp_min} ℃\nさいこうきおん: ${temp_max} ℃\nしつど: ${humidity} %\n気圧: ${pressure} hPa`)
-      })
-      for (let i = 0; i < wetherProperty.length; i++) {
-        console.log(`Number: ${i}`)
-        console.log(`Message: ${wetherProperty[i]}`)
-        await line
-          .sendMessage(replyToken, wetherProperty[i])
+router.post('/textMessage', (req, res, next) => {
+  res.status(200).end()
+
+  const message = req.body
+  message.map(item => {
+    const searchType = item.message.text
+    const replyToken = item.replyToken
+
+    switch (searchType) {
+      case '明日の天気':
+        const actLocation = {
+          items: [
+            {
+              type: 'action',
+              action: {
+                type: 'location',
+                label: '地図をから選択'
+              }
+            }
+          ]
+        }
+        line
+          .sendQuickMessage(
+            replyToken,
+            '調べたい場所を教えてください',
+            actLocation
+          )
           .then(result => console.log(result.data))
           .catch(err => console.error(err.response.data))
-        await sleep(1000)
-      }
-    } catch (err) {
-      console.error(err)
+        break
     }
   })
+})
+
+router.get('/weather', async (req, res, next) => {
+  try {
+    const geoIp = qs.parse(req.query)
+    const weather = await wth.describe(geoIp.latitude, geoIp.longitude)
+    let wetherProperty = weather.data.list.filter(list => {
+      return moment.unix(list.dt).format('HH') === '09'
+    })
+    wetherProperty = wetherProperty.map(list => ({
+      date: moment.unix(list.dt).format('YYYYMMDD'),
+      temp_min: Math.floor(parseFloat(list.main.temp_min) - 273.15),
+      temp_max: Math.floor(parseFloat(list.main.temp_max) - 273.15),
+      humidity: list.main.humidity,
+      pressure: list.main.pressure,
+      weatherDesc: wth.wetherDescription(list.weather[0].icon)
+    }))
+    console.log(wetherProperty)
+    res.status(200).json({ weekly: wetherProperty })
+  } catch (err) {
+    console.error(err)
+    res.status(400).json('error happened')
+  }
 })
 
 module.exports = router
